2809ca796f160ec5a19072138791e4c4170f2755
Tony Sims <zocle@zocle.net>
2021-03-19 18:50:16 +0000

    fixed

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" 12. Type Checking """
+
+from typing import List
+
+
+def zoom_array(lst: List, factor: int = 2) -> List:
+    ''' zoom array, provided for demonstration '''
+
+    zoomed_in: List = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array = [12, 72, 91]
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
