77e3bbb4db7fe956616084800b8ed0331cfed4e4
Tony Sims <zocle@zocle.net>
2021-03-24 16:07:47 +0000

    changed page size to length of data

--- 0x04-pagination/2-hypermedia_pagination.py
+++ 0x04-pagination/2-hypermedia_pagination.py
@@ -40,7 +40,7 @@ class Server:
         total_pages = math.ceil(int(len(self.dataset())) / page_size)
 
         return {
-            "page_size": page_size,
+            "page_size": len(self.get_page(page, page_size)),
             "page": page,
             "data": self.get_page(page, page_size),
             "next_page": page + 1 if page < total_pages else None,
