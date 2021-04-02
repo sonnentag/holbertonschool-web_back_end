84697e4218180169420b538498a9fb6919fa2116
Tony Sims <zocle@zocle.net>
2021-04-02 04:07:18 +0000

    initial push

---
+++ 0x06-Basic_authentication/README.md
@@ -0,0 +1,3 @@
+## 0x06-Basic_authentication
+###
+### - implement a Basic Authentication on a simple API.
---
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+""" Auth module
+"""
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth():
+    """ Auth Class
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ require_auth returns false if path is excluded
+        """
+        if not path or not excluded_paths or excluded_paths is []:
+            return True
+        if path[len(path) - 1] is not '/':
+            path += '/'
+        if path in excluded_paths:
+            return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """ returns auth header
+        """
+        if request and request.headers.get('Authorization'):
+            return request.headers.get('Authorization')
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ returns None for now
+        """
+        return None
