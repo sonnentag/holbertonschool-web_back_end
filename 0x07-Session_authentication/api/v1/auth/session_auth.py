7b6850abf3908ae9c2f3263d139d206d901c1bdf
Tony Sims <zocle@zocle.net>
2021-04-11 23:08:57 +0000

    updated comments

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -12,23 +12,23 @@ class SessionAuth(Auth):
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
+        """ Create new session
         """
-        """
-        if user_id is None or type(user_id) not str:
+        if user_id is None or not isinstance(user_id, str):
             return None
         session_id = str(uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
+        """ Return user id of current user
         """
-        """
-        if session_id is None or type(session_id) not str:
+        if session_id is None or not isinstance(user_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
 
     def session_cookie(self, request=None):
-        """
+        """ Return current session cookie
         """
         if request:
             return request.cookies.get(os.getenv("SESSION_NAME"))
