046e922ce7d9d0aa161d44e8d19c38d712996799
Tony Sims <zocle@zocle.net>
2021-04-12 03:23:45 +0000

    fixed typo

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -23,7 +23,7 @@ class SessionAuth(Auth):
     def user_id_for_session_id(self, session_id: str = None) -> str:
         """ Return user id of current user
         """
-        if session_id is None or not isinstance(user_id, str):
+        if session_id is None or not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
 
