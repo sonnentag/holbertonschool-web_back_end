2aa05e120eaa06226d309203a3034de8b269da41
Tony Sims <zocle@zocle.net>
2021-04-21 08:59:28 +0000

    correcting 1

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -30,7 +30,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str) -> TypeVar('User'):
+    def add_user(self, email: str, hashed_password: str) -> User:
         """ add user
         """
         user = User(email=email, hashed_password=hashed_password)
