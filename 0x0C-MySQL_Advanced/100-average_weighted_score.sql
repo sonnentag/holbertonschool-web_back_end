a9427a3f1496d637210565c6eeda9a9fece95e64
Tony Sims <zocle@zocle.net>
2021-04-30 04:34:20 +0000

    100

--- 0x0C-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x0C-MySQL_Advanced/100-average_weighted_score.sql
@@ -1,4 +1,4 @@
--- 
+--  12. Average weighted score
 --
 
 DELIMITER $$ 
@@ -9,13 +9,13 @@ CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id INT)
 BEGIN
   UPDATE users 
     SET average_score = (
-       SELECT sum(weight)/sum(x.weights) AS average FROM (
+       SELECT sum(weight)/sum(x.weights) FROM (
 	  SELECT p.weight, (count(p.weight) * p.weight) AS weights 
 	    FROM projects p, corrections c 
 	    WHERE c.user_id = user_id 
             GROUP BY p.weight) x)
       WHERE id = user_id;
-END$$
+END
+$$
 
-DELIMITER
-;
+DELIMITER ;
