7c2d5d5612c9545a90dc3fb71fdf4dde7db328bc
Tony Sims <zocle@zocle.net>
2021-04-30 02:17:17 +0000

    missing comma

--- 0x0C-MySQL_Advanced/1-country_users.sql
+++ 0x0C-MySQL_Advanced/1-country_users.sql
@@ -3,6 +3,6 @@
 CREATE TABLE If NOT EXISTS `users` (  
 	`id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	`email` VARCHAR(255) NOT NULL UNIQUE,
-	`name` VARCHAR(255)
+	`name` VARCHAR(255),
 	`country` ENUM('US', 'CO', 'TN') NOT NULL
 );
