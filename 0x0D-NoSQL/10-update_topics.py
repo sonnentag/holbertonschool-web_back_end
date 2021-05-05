68e56a6d55694c65e7ca991cf0984a677b67db24
Tony Sims <zocle@zocle.net>
2021-05-05 00:57:11 +0000

    quotations

--- 0x0D-NoSQL/10-update_topics.py
+++ 0x0D-NoSQL/10-update_topics.py
@@ -2,12 +2,13 @@
 """ 10. Change school topics
 """
 import pymongo
+from typing import List
 
 
 def update_topics(mongo_collection, name, topics):
     """ changes all topics of doc based on name
     """
     mongo_collection.update_many(
-        {name: name},
-        {$set: {topics: topics}}
+        {"name": name},
+        {"$set": {"topics": topics}}
     )
