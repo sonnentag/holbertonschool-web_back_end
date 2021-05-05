afb92dd41e671a458c47cbc191e66422ab6f8a11
Tony Sims <zocle@zocle.net>
2021-05-05 01:52:04 +0000

    if main

--- 0x0D-NoSQL/12-log_stats.py
+++ 0x0D-NoSQL/12-log_stats.py
@@ -3,14 +3,16 @@
 """
 from pymongo import MongoClient
 
+if __name__ == "__main__":
+    client = MongoClient('mongodb://localhost:27017/')
+    db = client.logs.nginx
+    method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
-client = MongoClient('mongodb://localhost:27017/')
-db = client.logs.nginx
-method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print(db.count_documents({}), "logs")
+    print("Methods:")
+    for m in method:
+        print(f'\tmethod {m}: db.countDocuments()')
 
-print(db.count_documents({}), "logs")
-print("Methods:")
-for m in method:
-    print(f'\tmethod {m}: db.countDocuments()')
-
-print(db.count_documents({"method": "GET", "path": "/status"}) "status check")
+    print(db.count_documents(
+        {"method": "GET", "path": "/status"}
+        ) "status check")
