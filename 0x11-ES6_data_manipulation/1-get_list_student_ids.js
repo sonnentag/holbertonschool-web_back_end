a3dc95161c613baf4f7cf7582e697e175594e4af
root <root@packer-output-4561104f-c8dd-462f-916d-4beaff0dbc86>
2021-05-19 01:22:05 +0000

    let

--- 0x11-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x11-ES6_data_manipulation/1-get_list_student_ids.js
@@ -2,6 +2,6 @@ const getListStudentIds = (objectArray) => {
   if (!Array.isArray(objectArray)) {
     return [];
   }
-  const idList = objectArray.map((_) => _.id);
+  let idList = objectArray.map((_) => _.id);
   return idList;
 };
