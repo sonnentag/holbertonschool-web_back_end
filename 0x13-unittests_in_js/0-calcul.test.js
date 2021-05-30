0659a30fa49b4641310f543d83b1cf6828a03e40
root <root@packer-output-4561104f-c8dd-462f-916d-4beaff0dbc86>
2021-05-30 18:38:05 +0000

    first

---
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+const calculateNumber = require("./0-calcul.js");
+
+describe('calculateNumber', function() {
+  it('1st test for correct output should return 4', function() {
+    assert.strictEqual(calculateNumber(1, 3), 4);
+    });
+  it('2nd test with rouding should return 5', function() {
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+    });
+  it('3rd test with rouding should return 5', function() {
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    });
+  it('4th test with rouding should return 6', function() {
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+    });
+});
