507f569350ade1c6a70f305070b82966de2d833d
root <root@packer-output-4561104f-c8dd-462f-916d-4beaff0dbc86>
2021-06-05 04:03:02 +0000

    changed string repr.3

--- 0x13-unittests_in_js/9-api/api.js
+++ 0x13-unittests_in_js/9-api/api.js
@@ -4,8 +4,8 @@ const app = express();
 app.get('/', (request, result) => {
   result.end('Welcome to the payment system');
 });
-app.get('/cart/:id[0-9]+', (request, result) => {
-  result.end('Payment methods for cart '`${request.params.id}`);
+app.get('/cart/:id([0-9]+)', (request, result) => {
+  result.end(`Payment methods for cart ${id}`);
 });
 app.listen(7865, () => {
   console.log('API available on localhost port 7865');
