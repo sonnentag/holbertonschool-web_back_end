2ca21b00d0d2a4707ac16b95c7a88a96b3b5f0f9
root <root@packer-output-4561104f-c8dd-462f-916d-4beaff0dbc86>
2021-06-05 02:08:15 +0000

    added div and sub'

--- 0x13-unittests_in_js/1-calcul.test.js
+++ 0x13-unittests_in_js/1-calcul.test.js
@@ -46,3 +46,101 @@ describe('up down', function() {
     assert.strictEqual(calculateNumber('SUM', 1.5, 3.3), 5);
     });
 });
+
+describe('no rounding', function() {
+  it('1st test for correct output should return 4', function() {
+    assert.strictEqual(calculateNumber('DIVIDE', 12, 3), 4);
+    });
+});
+describe('round first down', function() {
+  it('2nd test with rounding should return 4', function() {
+    assert.strictEqual(calculateNumber('DIVIDE', 12.2, 3), 4);
+    });
+});
+describe('round first up', function() {
+  it('3rd test with rounding should return 5', function() {
+    assert.strictEqual(calculateNumber('DIVIDE', 14.6, 3), 5);
+    });
+});
+describe('round second down', function() {
+  it('4th test with rounding should return 4', function() {
+    assert.strictEqual(calculateNumber('DIVIDE', 12, 3.4), 4);
+    });
+});
+describe('round second up', function() {
+  it('5th test with rounding should return 5', function() {
+    assert.strictEqual(calculateNumber('DIVIDE', 20, 3.7), 5);
+    });
+});
+describe('round both down', function() {
+  it('6th test with rounding should return 4', function() {
+    assert.strictEqual(calculateNumber('DIVIDE', 12.3, 3.2), 4);
+    });
+});
+describe('round both up', function() {
+  it('7th test with rounding should return 6', function() {
+    assert.strictEqual(calculateNumber('DIVIDE', 23.5, 3.7), 6);
+    });
+});
+describe('down up', function() {
+  it('8th test with rounding should return 5', function() {
+    assert.strictEqual(calculateNumber('DIVIDE', 20.3, 3.7), 5);
+    });
+});
+describe('up down', function() {
+  it('9th test with rounding should return 5', function() {
+    assert.strictEqual(calculateNumber('DIVIDE', 19.5, 4.3), 5);
+    });
+});
+describe('division by 0 error', function() {
+  it('10th division test by 0 should error', function() {
+    assert.strictEqual(calculateNumber('DIVIDE', 4, 0), 'Error');
+  });
+});
+
+describe('no rounding', function() {
+  it('1st test for correct output should return 4', function() {
+    assert.strictEqual(calculateNumber('SUBTRACT', 7, 3), 4);
+    });
+});
+
+describe('round first down', function() {
+  it('2nd test with rounding should return 4', function() {
+    assert.strictEqual(calculateNumber('SUBTRACT', 7.2, 3), 4);
+    });
+});
+describe('round first up', function() {
+  it('3rd test with rounding should return 5', function() {
+    assert.strictEqual(calculateNumber('SUBTRACT', 7.6, 3), 5);
+    });
+});
+describe('round second down', function() {
+  it('4th test with rounding should return 4', function() {
+    assert.strictEqual(calculateNumber('SUBTRACT', 7, 3.4), 4);
+    });
+});
+describe('round second up', function() {
+  it('5th test with rounding should return 5', function() {
+    assert.strictEqual(calculateNumber('SUBTRACT', 9, 3.7), 5);
+    });
+});
+describe('round both down', function() {
+  it('6th test with rounding should return 4', function() {
+    assert.strictEqual(calculateNumber('SUBTRACT', 7.3, 3.2), 4);
+    });
+});
+describe('round both up', function() {
+  it('7th test with rounding should return 6', function() {
+    assert.strictEqual(calculateNumber('SUBTRACT', 9.5, 3.7), 6);
+    });
+});
+describe('down up', function() {
+  it('8th test with rounding should return 5', function() {
+    assert.strictEqual(calculateNumber('SUBTRACT', 9.3, 3.7), 5);
+    });
+});
+describe('up down', function() {
+  it('9th test with rounding should return 5', function() {
+    assert.strictEqual(calculateNumber('SUBTRACT', 7.5, 3.3), 5);
+    });
+});
--- 0x13-unittests_in_js/2-calcul_chai.test.js
+++ 0x13-unittests_in_js/2-calcul_chai.test.js
@@ -46,3 +46,100 @@ describe('up down', function() {
     chai.expect(calculateNumber('SUM', 1.5, 3.3)).to.equal(5);
     });
 });
+
+describe('no rounding', function() {
+  it('1st test for correct output should return 4', function() {
+    chai.expect(calculateNumber('DIVIDE', 12, 3)).to.equal(4);
+    });
+});
+describe('round first down', function() {
+  it('2nd test with rounding should return 4', function() {
+    chai.expect(calculateNumber('DIVIDE', 12.2, 3)).to.equal(4);
+    });
+});
+describe('round first up', function() {
+  it('3rd test with rounding should return 5', function() {
+    chai.expect(calculateNumber('DIVIDE', 14.6, 3)).to.equal(5);
+    });
+});
+describe('round second down', function() {
+  it('4th test with rounding should return 4', function() {
+    chai.expect(calculateNumber('DIVIDE', 12, 3.4)).to.equal(4);
+    });
+});
+describe('round second up', function() {
+  it('5th test with rounding should return 5', function() {
+    chai.expect(calculateNumber('DIVIDE', 15, 2.7)).to.equal(5);
+    });
+});
+describe('round both down', function() {
+  it('6th test with rounding should return 4', function() {
+    chai.expect(calculateNumber('DIVIDE', 12.3, 3.2)).to.equal(4);
+    });
+});
+describe('round both up', function() {
+  it('7th test with rounding should return 6', function() {
+    chai.expect(calculateNumber('DIVIDE', 17.5, 2.7)).to.equal(6);
+    });
+});
+describe('down up', function() {
+  it('8th test with rounding should return 5', function() {
+    chai.expect(calculateNumber('DIVIDE', 15.3, 2.7)).to.equal(5);
+    });
+});
+describe('up down', function() {
+  it('9th test with rounding should return 5', function() {
+    chai.expect(calculateNumber('DIVIDE', 14.5, 3.3)).to.equal(5);
+    });
+});
+describe('division by 0 error', function() {
+  it('10th division test by 0 should error', function() {
+    chai.expect(calculateNumber('DIVIDE', 4, 0)).to.equal('Error');
+  });
+});
+
+describe('no rounding', function() {
+  it('1st test for correct output should return 4', function() {
+    chai.expect(calculateNumber('SUBTRACT', 7, 3)).to.equal(4);
+    });
+});
+describe('round first down', function() {
+  it('2nd test with rounding should return 4', function() {
+    chai.expect(calculateNumber('SUBTRACT', 7.2, 3)).to.equal(4);
+    });
+});
+describe('round first up', function() {
+  it('3rd test with rounding should return 5', function() {
+    chai.expect(calculateNumber('SUBTRACT', 7.6, 3)).to.equal(5);
+    });
+});
+describe('round second down', function() {
+  it('4th test with rounding should return 4', function() {
+    chai.expect(calculateNumber('SUBTRACT', 7, 3.4)).to.equal(4);
+    });
+});
+describe('round second up', function() {
+  it('5th test with rounding should return 5', function() {
+    chai.expect(calculateNumber('SUBTRACT', 9, 3.7)).to.equal(5);
+    });
+});
+describe('round both down', function() {
+  it('6th test with rounding should return 4', function() {
+    chai.expect(calculateNumber('SUBTRACT', 7.3, 3.2)).to.equal(4);
+    });
+});
+describe('round both up', function() {
+  it('7th test with rounding should return 6', function() {
+    chai.expect(calculateNumber('SUBTRACT', 9.5, 3.7)).to.equal(6);
+    });
+});
+describe('down up', function() {
+  it('8th test with rounding should return 5', function() {
+    chai.expect(calculateNumber('SUBTRACT', 9.3, 3.7)).to.equal(5);
+    });
+});
+describe('up down', function() {
+  it('9th test with rounding should return 5', function() {
+    chai.expect(calculateNumber('SUBTRACT', 7.5, 3.3)).to.equal(5);
+    });
+});
